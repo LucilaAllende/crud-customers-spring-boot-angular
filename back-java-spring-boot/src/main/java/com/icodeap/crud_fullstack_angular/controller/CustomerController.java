fd5246e5c30ad11b1f073a9ef96485ea7918a5a1
LucilaAllende <abigail_lucii@hotmail.com>
2025-02-24 16:41:58 -0300

    add CustomerController for managing customer CRUD operations

---
+++ back-java-spring-boot/src/main/java/com/icodeap/crud_fullstack_angular/controller/CustomerController.java
@@ -0,0 +1,47 @@
+package com.icodeap.crud_fullstack_angular.controller;
+
+import com.icodeap.crud_fullstack_angular.entity.Customer;
+import com.icodeap.crud_fullstack_angular.service.CustomerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public Customer save(@RequestBody Customer customer) {
+        return customerService.save(customer);
+    }
+
+    @GetMapping
+    public List<Customer> findAll() {
+        return customerService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Customer findById(@PathVariable Integer id) {
+        return customerService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        customerService.deleteById(id);
+    }
+
+    @PutMapping
+    public Customer update(@RequestBody Customer customer) {
+        Customer customerDb = customerService.findById(customer.getId());
+        customerDb.setFirstName(customer.getFirstName());
+        customerDb.setLastName(customer.getLastName());
+        customerDb.setEmail(customer.getEmail());
+        return customerService.update(customerDb);
+    }
+}
