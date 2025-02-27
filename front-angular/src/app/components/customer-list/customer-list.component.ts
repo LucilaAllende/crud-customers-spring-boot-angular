239327702a2d992bf1932b4bb6909e0f92f40975
LucilaAllende <abigail_lucii@hotmail.com>
2025-02-27 16:47:25 -0300

    add customer list component with template, styles, and tests

---
+++ front-angular/src/app/components/customer-list/customer-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-list',
+  imports: [],
+  templateUrl: './customer-list.component.html',
+  styleUrl: './customer-list.component.css'
+})
+export class CustomerListComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
