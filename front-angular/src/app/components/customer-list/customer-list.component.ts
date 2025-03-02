2f5ddc339833fd02b71d6526bd1d7594245b0c0c
LucilaAllende <abigail_lucii@hotmail.com>
2025-03-02 11:29:56 -0300

    implement customer listing in CustomerListComponent with API integration

--- front-angular/src/app/components/customer-list/customer-list.component.ts
+++ front-angular/src/app/components/customer-list/customer-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../../service/customer.service';
+import { Customer } from '../../class/customer';
 
 @Component({
   selector: 'app-customer-list',
   imports: [],
+  standalone: true,
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
 export class CustomerListComponent implements OnInit {
-  constructor() { }
+
+  customers: Customer[] = [];
+
+  constructor(private customerService: CustomerService) { }
 
   ngOnInit() {
+    this.listCustomers();
+  }
+
+  listCustomers() {
+    this.customerService.getCustomers().subscribe(
+      data => {
+        console.log('Customers=', data);
+        this.customers = data;
+        console.log('Customers=', this.customers);
+      }
+    );
   }
 
 }
