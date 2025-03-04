ded3e7a94b4bc381e5958c16cc309d75d16dc251
LucilaAllende <abigail_lucii@hotmail.com>
2025-03-04 18:15:40 -0300

    add CORS support to CustomerController for local development

--- back-java-spring-boot/src/main/java/com/icodeap/crud_fullstack_angular/controller/CustomerController.java
+++ back-java-spring-boot/src/main/java/com/icodeap/crud_fullstack_angular/controller/CustomerController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
 
     private final CustomerService customerService;
