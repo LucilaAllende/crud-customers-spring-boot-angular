b12ad04ed9a41356cb37f49f5d5556fc27582f30
LucilaAllende <abigail_lucii@hotmail.com>
2025-03-09 10:38:51 -0300

    refactor Customer class to make id optional in constructor

--- front-angular/src/app/class/customer.ts
+++ front-angular/src/app/class/customer.ts
@@ -1,8 +1,8 @@
 export class Customer {
   constructor(
-    public id: number,
     public firstName: string,
     public lastName: string,
-    public email: string
+    public email: string,
+    public id?: number,
   ) {}
 }
