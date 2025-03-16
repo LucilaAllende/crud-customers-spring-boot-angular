287d6ceaa8cf9e824b0ab307e61d1f559b6d5066
LucilaAllende <abigail_lucii@hotmail.com>
2025-03-16 16:26:22 -0300

    add config unit tests for CustomerService with HTTP client testing setup

--- front-angular/src/app/service/customer.service.spec.ts
+++ front-angular/src/app/service/customer.service.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CustomerService } from './customer.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { Customer } from '../class/customer';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('CustomerService', () => {
   let service: CustomerService;
+  let httpMock: HttpTestingController;
+  const apiUrl = 'http://localhost:8080/customers';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [CustomerService, provideHttpClient(), provideHttpClientTesting()]
+    });
+
     service = TestBed.inject(CustomerService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verifica que no haya solicitudes HTTP pendientes
   });
 
   it('should be created', () => {
