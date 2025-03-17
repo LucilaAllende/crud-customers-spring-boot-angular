0a3d84fd03f7f86e69498fc6eedb5f23e4716c34
LucilaAllende <abigail_lucii@hotmail.com>
2025-03-17 17:39:18 -0300

    add router provider to AppComponent test and update title test to check for router-outlet

--- front-angular/src/app/app.component.spec.ts
+++ front-angular/src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -17,16 +18,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'front-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-angular');
-  });
-
-  it('should render title', () => {
+  it('should render the router-outlet', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, front-angular');
+    
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
