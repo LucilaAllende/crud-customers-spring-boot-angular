766b41796aef5a42f69c6f7407a5c0c3eb2b8767
LucilaAkkllende <abigail_lucii@hotmail.com>
2025-05-07 12:03:08 -0300

    add logic for control theme in app component ts

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -10,4 +10,19 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  currentTheme: string = 'auto';
+
+  constructor(private themeService: ThemeService) { }
+  
+
+  ngOnInit() {
+    this.themeService.getThemeObservable().subscribe(theme => {
+      this.currentTheme = theme;
+    });
+  }
+
+  changeTheme(theme: 'light' | 'dark' | 'auto') {
+    this.currentTheme = theme;
+    this.themeService.setTheme(theme);
+  }
 }
